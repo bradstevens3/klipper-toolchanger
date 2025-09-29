2e5db3fdf527dfcb6358a0bc8b4c933e816b3268
bradstevens3 <bradstevens3@gmail.com>
2025-09-29 11:46:29 -0500

    Update toolchanger.py

--- klipper/extras/toolchanger.py
+++ klipper/extras/toolchanger.py
@@ -211,16 +211,18 @@ class Toolchanger:
         heaters.set_temperature(tool.extruder.get_heater(), temp, wait=False)
         if wait_cmd:
             gcmd.respond_info("extruder_name %s" % (extruder_name))
-            extruder = tool.extruder
+            #extruder = tool.extruder
             #extruder = self.printer.lookup_object(extruder_name)
-            gcmd.respond_info("extruder %s" % (extruder))
+            #gcmd.respond_info("extruder %s" % (extruder))
             #temp_info = extruder.get_temp()
             #gcmd.respond_info("temp_info %s" % (temp_info))
             #cur_temp = temp_info["temperature"]
-            cur_temp = tool.extruder.get_temp()["temperature"]
+            heater = tool.extruder.get_heater()     # Get the heater object
+            cur_temp = heater.get_temp()            # Get current temperature (float)
+            #cur_temp = tool.extruder.get_temp()["temperature"]
             gcmd.respond_info("cur_temp %s" % (cur_temp))
             #cur_temp = self.get_toolhead_temperature(tool, gcmd)
-            gcmd.respond_info(f"Current temp: {cur_temp:.1f}°C / Target: {temp:.1f}°C")
+            #gcmd.respond_info(f"Current temp: {cur_temp:.1f}°C / Target: {temp:.1f}°C")
 
     def _get_tool_from_gcmd(self, gcmd):
         tool_name = gcmd.get('TOOL', None)
